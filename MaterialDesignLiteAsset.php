b215a9c810e80b56c14df1ef5cb581da9671c4e8
rafaelvaloto <rafaelvaloto@gmail.com>
2015-12-17 00:13:33 -0200

    source path

--- MaterialDesignLiteAsset.php
+++ MaterialDesignLiteAsset.php
@@ -8,7 +8,7 @@ use yii\web\AssetBundle;
  **/
 class MaterialDesignLiteAsset extends AssetBundle
 {
-	public 	$sourcePath = '@vendor/bower/bowerAsset/material-design-lite';
+	public 	$sourcePath = '@vendor/bower/bower-asset/material-design-lite';
 
 	public $css = [
 		'material-design-lite.min.css'	
